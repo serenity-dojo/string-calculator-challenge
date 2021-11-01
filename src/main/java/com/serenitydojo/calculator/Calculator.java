0238ea5fe68927a83785328b9068a48694404c6f
Elena Rodriguez <helenarodcal@gmail.com>
2021-11-01 16:14:19 +0100

    Integer Calculator

--- src/main/java/com/serenitydojo/calculator/Calculator.java
+++ src/main/java/com/serenitydojo/calculator/Calculator.java
@@ -1,7 +1,42 @@
 package com.serenitydojo.calculator;
 
+import com.google.common.base.Splitter;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+
 public class Calculator {
     public int evaluate(String expression) {
-        return 0;
+        if(expression.isEmpty()){
+            return 0;
+        }
+
+        List<String> characters = Splitter.on(" ").splitToList(expression);
+
+        int total = 0;
+        String operator = "+";
+
+        for(String character : characters){
+            if(StringUtils.isNumeric(character)) {
+                total = execute(total, operator, character);
+            } else {
+                operator =  character;
+            }
+        }
+
+        return total;
+    }
+
+    private int execute(int total, String operator, String character) {
+        switch (operator) {
+            case "+":
+                return total + Integer.parseInt(character);
+            case "-":
+                return total - Integer.parseInt(character);
+            case "*":
+                return total * Integer.parseInt(character);
+            default:
+                throw new IllegalMathsOperatorException("Operator " + operator + " not allowed");
+        }
     }
 }
