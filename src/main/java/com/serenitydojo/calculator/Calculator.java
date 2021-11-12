5eb54884a63b8aa405f57fb157549418ef66aa8a
Karol <krl.michniewicz@gmail.com>
2021-11-12 19:17:15 +0100

    Completed string calculator, added a case for division.

--- src/main/java/com/serenitydojo/calculator/Calculator.java
+++ src/main/java/com/serenitydojo/calculator/Calculator.java
@@ -1,7 +1,40 @@
 package com.serenitydojo.calculator;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Calculator {
     public int evaluate(String expression) {
-        return 0;
+        if(expression.isEmpty()) {
+            return 0;
+        }
+
+        int calculationResult = 0;
+        String currentOperator = "+";
+
+        String[] characters = expression.split("\\s");
+
+        for(String character : characters) {
+            if (!StringUtils.isNumeric(character)) {
+                currentOperator = character;
+            } else {
+                calculationResult = calculation(calculationResult, currentOperator, character);
+            }
+        }
+        return calculationResult;
+    }
+
+    private int calculation(int calculationResult, String currentOperator, String character) throws IllegalMathsOperatorException {
+        switch(currentOperator) {
+            case "+":
+                return calculationResult + Integer.parseInt(character);
+            case "-":
+                return calculationResult - Integer.parseInt(character);
+            case "*":
+                return calculationResult * Integer.parseInt(character);
+            case "/":
+                return calculationResult / Integer.parseInt(character);
+            default:
+                throw new IllegalMathsOperatorException(currentOperator + " operator is not supported!");
+        }
     }
 }
