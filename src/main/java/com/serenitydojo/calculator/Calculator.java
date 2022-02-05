0ffe0631647bd08cdeac7ca3dbc7a7b336fc4ee7
sai5ii <sai5i.islam@gmail.com>
2022-02-05 20:21:06 +0500

    Calculator by Sai5ii
    Challenge 2

--- src/main/java/com/serenitydojo/calculator/Calculator.java
+++ src/main/java/com/serenitydojo/calculator/Calculator.java
@@ -1,7 +1,50 @@
 package com.serenitydojo.calculator;
+import java.util.Arrays;
+import java.util.List;
+import static org.apache.commons.lang3.StringUtils.isNumeric;
 
 public class Calculator {
+
     public int evaluate(String expression) {
-        return 0;
+        //String[] expressionToArray = expression.split(" ");
+
+        if (expression.isEmpty()) {
+            return 0;
+        }
+
+        String[] tokens = expression.split(" ");
+
+        int currTotal = 0;
+        String nextOperator = "+";
+
+        for (String token : tokens) {
+            if (!isNumeric(token)) {
+                nextOperator = token;
+
+            } else {
+                currTotal = process(currTotal,nextOperator,token);
+
+            }
+        }
+        return currTotal;
     }
+
+        private int process(int currTotal, String nextOperator, String token) {
+            switch (nextOperator){
+                case "+":
+                    return  currTotal + Integer.parseInt(token);
+                case "-":
+                    return  currTotal - Integer.parseInt(token);
+                case "*":
+                    return  currTotal * Integer.parseInt(token);
+                case "/":
+                    return  currTotal / Integer.parseInt(token);
+                default:
+                    throw new IllegalMathsOperatorException("Unknown operator " + nextOperator);
+            }
+        }
 }
+
+
+
+
