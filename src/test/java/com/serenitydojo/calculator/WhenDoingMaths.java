e0ac345ba2e7b1e0ec59f83ebd90e93d1c2b140b
Cosmin Pop <cosmin@cosminpop.uk>
2022-03-15 18:03:23 +0000

    solution - problems with exception

--- src/test/java/com/serenitydojo/calculator/WhenDoingMaths.java
+++ src/test/java/com/serenitydojo/calculator/WhenDoingMaths.java
@@ -3,48 +3,50 @@ package com.serenitydojo.calculator;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
+import javax.script.ScriptException;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WhenDoingMaths {
     Calculator calculator = new Calculator();
 
     @Test
-    public void shouldReturnZeroForAnEmptyString() {
+    public void shouldReturnZeroForAnEmptyString() throws Exception {
         assertThat(calculator.evaluate("")).isEqualTo(0);
     }
+//
+    @Test
+    public void shouldReportNonSupportedOperations() throws Exception {
+        calculator.evaluate("1 ^ 2");
+    }
+//
+    @Test
+    public void shouldReturnTheValueOfASingleNumber() throws Exception {
+        assertThat(calculator.evaluate("1")).isEqualTo(1);
+    }
+////
+    @Test
+    public void shouldAddTwoNumbers() throws Exception {
+        assertThat(calculator.evaluate("1 + 1")).isEqualTo(2);
+    }
+//
+    @Test
+    public void shouldAddThreeNumbers() throws Exception {
+        assertThat(calculator.evaluate("1 + 2 + 3")).isEqualTo(6);
+    }
 
-//    @Test(expected = IllegalMathsOperatorException.class)
-//    public void shouldReportNonSupportedOperations() {
-//        calculator.evaluate("1 ^ 2");
-//    }
-//
-//    @Test
-//    public void shouldReturnTheValueOfASingleNumber() {
-//        assertThat(calculator.evaluate("1")).isEqualTo(1);
-//    }
-//
-//    @Test
-//    public void shouldAddTwoNumbers() {
-//        assertThat(calculator.evaluate("1 + 1")).isEqualTo(2);
-//    }
-//
-//    @Test
-//    public void shouldAddThreeNumbers() {
-//        assertThat(calculator.evaluate("1 + 2 + 3")).isEqualTo(6);
-//    }
-//
-//    @Test
-//    public void shouldAlsoSubtract() {
-//        assertThat(calculator.evaluate("10 - 6")).isEqualTo(4);
-//    }
-//
-//    @Test
-//    public void shouldAddAndSubtract() {
-//        assertThat(calculator.evaluate("10 + 5 - 6")).isEqualTo(9);
-//    }
-//
-//    @Test
-//    public void shouldMultiplyNumbers() {
-//        assertThat(calculator.evaluate("10 * 5")).isEqualTo(50);
-//    }
+    @Test
+    public void shouldAlsoSubtract() throws Exception {
+        assertThat(calculator.evaluate("10 - 6")).isEqualTo(4);
+    }
+
+    @Test
+    public void shouldAddAndSubtract() throws Exception {
+        assertThat(calculator.evaluate("10 + 5 - 6")).isEqualTo(9);
+    }
+
+    @Test
+    public void shouldMultiplyNumbers() throws Exception {
+        assertThat(calculator.evaluate("10 * 5")).isEqualTo(50);
+    }
 }
