b04ced246130f5b46b870b93ccb37bd3310968df
David Caboni <davcab+github@gmail.com>
2022-03-27 15:18:19 +0200

    first refacto

--- src/main/java/com/serenitydojo/calculator/Calculator.java
+++ src/main/java/com/serenitydojo/calculator/Calculator.java
@@ -5,13 +5,34 @@ import static org.apache.commons.lang3.StringUtils.isNumeric;
 public class Calculator {
 
     public int evaluate(String expression) {
-        int evaluation = 0;
-
         if (!expression.isEmpty()) {
+            return preparingTheCalculation(expression);
+        }
+
+        return 0;
+    }
+
+
+    private int doingTheOperation(String operator, int number1, int number2) {
+        switch (operator) {
+            case "+":
+                return number1 + number2;
+            case "-":
+                return number1 - number2;
+            case "/":
+                return number1 / number2;
+            case "*":
+                return number1 * number2;
+            default:
+                return number1;
+        }
+    }
+
+    private int preparingTheCalculation(String expression) {
+            int evaluation = 0;
             String[] elements = expression.split(" ");
             int number1 = 0;
             int number2 = 0;
-            int res = 0;
             String operator = "";
             boolean firstTime = true;
 
@@ -20,10 +41,10 @@ public class Calculator {
                     if(firstTime == true) {
                         number1 = Integer.parseInt(element);
                         firstTime = false;
-                        if(res != 0) {
+                        if(number1 != 0 && number2 != 0) {
                             int intervertion = 0;
                             intervertion = number1;
-                            number1 = res;
+                            number1 = evaluation;
                             number2 = intervertion;
                         }
                     }
@@ -31,37 +52,21 @@ public class Calculator {
                         number2 = Integer.parseInt(element);
                         firstTime = true;
 
-                        if(res != 0) {
-                            number1 = res;
+                        if(number1 != 0) {
+                            number1 = evaluation;
                         }
                     }
 
-                    switch (operator) {
-                        case "+":
-                            res = res + number2;
-                            break;
-                        case "-":
-                            res = res - number2;
-                            break;
-                        case "/":
-                            res = res / number2;
-                            break;
-                        case "*":
-                            res = res * number2;
-                            break;
-                        default:
-                            res = number1 ;
-                            break;
-                    }
-                    evaluation = res;
+                    evaluation = doingTheOperation(operator, number1, number2);
                 } else {
                     operator = element;
                 }
             }
-        }
 
-        return evaluation;
+            return evaluation;
     }
+
+
 
 
 }
