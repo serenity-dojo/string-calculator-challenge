e41cc92275392f204796e35f8f513f9db5ed4873
David Caboni <davcab+github@gmail.com>
2022-03-27 19:01:17 +0200

    more refacto

--- src/main/java/com/serenitydojo/calculator/Calculator.java
+++ src/main/java/com/serenitydojo/calculator/Calculator.java
@@ -3,18 +3,19 @@ package com.serenitydojo.calculator;
 import static org.apache.commons.lang3.StringUtils.isNumeric;
 
 public class Calculator {
+    private String operator = "";
+    private int evaluation = 0;
 
     public int evaluate(String expression) {
         if (!expression.isEmpty()) {
-            return preparingTheCalculation(expression);
+            return iteratingThrewTheExpression(expression);
         }
 
         return 0;
     }
 
-
-    private int doingTheOperation(String operator, int number1, int number2) {
-        switch (operator) {
+    private int doingTheOperation( int number1, int number2) {
+        switch (this.operator) {
             case "+":
                 return number1 + number2;
             case "-":
@@ -28,46 +29,49 @@ public class Calculator {
         }
     }
 
-    private int preparingTheCalculation(String expression) {
-            int evaluation = 0;
-            String[] elements = expression.split(" ");
-            int number1 = 0;
-            int number2 = 0;
-            String operator = "";
-            boolean firstTime = true;
-
+    private int iteratingThrewTheExpression(String expression) {
+        String[] elements = expression.split(" ");
             for (String element : elements) {
-                if (isNumeric(element)) {
-                    if(firstTime == true) {
-                        number1 = Integer.parseInt(element);
-                        firstTime = false;
-                        if(number1 != 0 && number2 != 0) {
-                            int intervertion = 0;
-                            intervertion = number1;
-                            number1 = evaluation;
-                            number2 = intervertion;
-                        }
-                    }
-                    else {
-                        number2 = Integer.parseInt(element);
-                        firstTime = true;
-
-                        if(number1 != 0) {
-                            number1 = evaluation;
-                        }
-                    }
-
-                    evaluation = doingTheOperation(operator, number1, number2);
-                } else {
-                    operator = element;
-                }
+                preparingTheCalculation(element);
             }
 
-            return evaluation;
+            return this.evaluation;
+    }
+
+    private void preparingTheCalculation(String element) {
+        int number1 = 0;
+        int number2 = 0;
+
+        if (isNumeric(element)) {
+            int[] intFromString = gettingIntFromString(element);
+            this.evaluation = doingTheOperation(intFromString[0], intFromString[1]);
+        } else {
+            this.operator = element;
+        }
     }
 
 
+    private int[] gettingIntFromString(String element) {
+        int[] numberFromString = new int[2];
+        int number2 = 0;
 
+        if(this.evaluation == 0) {
+            int number1 = Integer.parseInt(element);
+            if(number1 != 0 && number2 != 0) {
+                number1 = this.evaluation;
+            }
+            numberFromString[0] = number1;
+        }
+        else {
+            number2 = Integer.parseInt(element);
+
+            if(this.evaluation != 0) {
+                numberFromString[0] = this.evaluation;
+            }
+            numberFromString[1] = number2;
+        }
+        return numberFromString;
+    }
 
 }
 
